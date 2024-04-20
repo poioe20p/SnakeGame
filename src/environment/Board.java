cbfbde176c9aafa6dc3ca34a043f05a6d12bbb36
mfidv-iscteiul <145684155+mfidv-iscteiul@users.noreply.github.com>
2024-04-20 23:46:38 +0100

    Finished selectPositionClosestToGoal in Board

--- src/environment/Board.java
+++ src/environment/Board.java
@@ -69,8 +69,13 @@ public abstract class Board extends Observable {
 	}
 
 	public BoardPosition selectPositionClosestToGoal(List<BoardPosition> possibleDestinations) {
-		//TODO
-		return null;
+		BoardPosition positionClosestToGoal = new BoardPosition(0, 0);
+		for(BoardPosition bp : possibleDestinations) {
+			if(bp.distanceTo(goalPosition) < positionClosestToGoal.distanceTo(goalPosition)) {
+				positionClosestToGoal = bp;
+			}
+		}
+		return positionClosestToGoal;
 	}
 
 	protected Goal addGoal() {
