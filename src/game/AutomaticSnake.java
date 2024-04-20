041b619dd86884bb8b423371bfa5949aedfd53b8
mfidv-iscteiul <145684155+mfidv-iscteiul@users.noreply.github.com>
2024-04-20 23:37:46 +0100

    Make run() method for AutomaticSnake

--- src/game/AutomaticSnake.java
+++ src/game/AutomaticSnake.java
@@ -12,14 +12,28 @@ import src.environment.Board;
 import src.environment.BoardPosition;
 
 public class AutomaticSnake extends Snake {
+	
 	public AutomaticSnake(int id, LocalBoard board) {
 		super(id,board);
-
 	}
 
 	@Override
 	public void run() {
-		//TODO
+		doInitialPositioning();
+		while(!wasKilled()) {
+			List<BoardPosition> possiblePositions = getBoard().getNeighboringPositions(getCells().get(0));
+			possiblePositions.removeIf(bp -> {
+				for(Cell c : this.getCells()){
+					if(c.getPosition().equals(getBoard().getCell(bp).getPosition())) {
+						return true;
+					}
+				}
+				return false;
+			});
+			try {
+				move(getBoard().getCell(getBoard().selectPositionClosestToGoal(possiblePositions)));
+			} catch (InterruptedException e) {e.printStackTrace();}
+		}
 	}
 	
 
